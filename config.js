6983bdf61f6eaf2882187055c704c2a5154b17e4
yadhavk15 <yadhavk15@gmail.com>
2021-05-13 01:34:03 +0400

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Yadhav Kumar Nundlall',
   githubUrl: 'https://github.com/yadhavk15/blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://yadhav-portfolio.netlify.app/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
